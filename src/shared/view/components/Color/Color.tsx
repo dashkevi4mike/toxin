ce82796e916a4dd3b4e71613c404518929be6131
Mikhail <dashkevi4mike@gmail.com>
2020-04-10 12:36:28 +0700

    tabs to spaces

--- src/shared/view/components/Color/Color.tsx
+++ src/shared/view/components/Color/Color.tsx
@@ -8,15 +8,15 @@ import './Color.scss';
 const b = block('color');
 
 function Color ({ hex, name, opacity }: ColorType) {
-	return (
-		<div className={b()}>
-			<div className={b('preview')} style={{ opacity, backgroundColor: hex, }}></div>
-			<div className={b('description')}>
-				<div className={b('name')}>{name}</div>
-				<div className={b('hex')}>{hex}</div> 
-			</div>
-		</div>
-	);
+  return (
+    <div className={b()}>
+      <div className={b('preview')} style={{ opacity, backgroundColor: hex, }}></div>
+      <div className={b('description')}>
+        <div className={b('name')}>{name}</div>
+        <div className={b('hex')}>{hex}</div> 
+      </div>
+    </div>
+  );
 }
   
 export { Color };
