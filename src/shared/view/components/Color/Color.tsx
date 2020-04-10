6ff3e0607f749eac678d08a16c4241fede99e531
Mikhail <dashkevi4mike@gmail.com>
2020-04-10 12:23:50 +0700

    colors list added and color mixin

---
+++ src/shared/view/components/Color/Color.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import block from 'bem-cn';
+
+import { ColorType } from 'shared/types/models';
+
+import './Color.scss';
+
+const b = block('color');
+
+function Color ({ hex, name, opacity }: ColorType) {
+	return (
+		<div className={b()}>
+			<div className={b('preview')} style={{ opacity, backgroundColor: hex, }}></div>
+			<div className={b('description')}>
+				<div className={b('name')}>{name}</div>
+				<div className={b('hex')}>{hex}</div> 
+			</div>
+		</div>
+	);
+}
+  
+  export { Color };
--- src/shared/view/components/Colors/Colors.tsx
+++ src/shared/view/components/Colors/Colors.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import block from 'bem-cn';
 
+import { colors } from 'shared/styles/colors';
+
+import { Color } from '../Color/Color';
+
 import './Colors.scss';
 
 const b = block('colors');
@@ -8,7 +12,16 @@ const b = block('colors');
 function Colors () {
   return (
     <div className={b()}>
-
+      {
+        colors.map((color)=> {
+          const { hex, name, opacity } = color;
+          return (
+            <div className={b('color')} key={name}>
+              <Color hex={hex} name={name} opacity={opacity} />
+            </div>
+          );
+        })
+      } 
     </div>
   );
 }
