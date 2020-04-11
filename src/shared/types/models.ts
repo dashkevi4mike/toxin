f6992e8cc42484a73e646012d42de22fa5bb74cc
Mikhail <dashkevi4mike@gmail.com>
2020-04-11 19:09:07 +0700

    adaptive for layout, but without burgermenu in header

--- src/shared/types/models.ts
+++ src/shared/types/models.ts
@@ -39,4 +39,4 @@ export type ColorType = { name: string; hex: string; opacity: number; }
 export type HeadlineType = { type: string; text: string; }
 export type SubLink = { title: string; href: string; };
 export type Link = { title: string; href: string; links?: SubLink[]};
-export type FooterLinksGroup = { title: string; links: Link[];}
+export type FooterLinksGroup = { title: string; links: Link[]; mobileInvisible: boolean; }
