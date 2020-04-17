c9a02d3be1c6734260bcdfa213e27814b3b45fdf
Mikhail <dashkevi4mike@gmail.com>
2020-04-17 23:14:22 +0700

    typings

--- src/shared/helpers/react/getFieldWithComponent.tsx
+++ src/shared/helpers/react/getFieldWithComponent.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Field, FieldProps as RFFieldProps } from 'react-final-form';
+import { Field } from 'react-final-form';
 // eslint-disable-next-line import/no-unresolved
-import { MergeRight } from '_helpers';
 
 
 type BaseWrappedFieldProps = {
@@ -9,22 +8,14 @@ type BaseWrappedFieldProps = {
   onChange?: any;
 };
 
-// keys for pick props from RFFieldProps, Omit dont work because RFFieldProps have index signature
-type RFFieldPropKey =
-  | 'allowNull' | 'format' | 'formatOnBlur' | 'parse' | 'name'
-  | 'isEqual' | 'subscription' | 'validate' | 'value';
-
 function getFieldWithComponent<P extends BaseWrappedFieldProps>(
   Component: React.ComponentType<P>,
 ) {
-  type OwnProps = Omit<P, keyof BaseWrappedFieldProps>;
-  type FieldProps = Pick<RFFieldProps<any>, RFFieldPropKey>;
-  type ResultProps = MergeRight<OwnProps, FieldProps>;
 
-  const result: React.StatelessComponent<ResultProps> = (props: ResultProps) => (
+  const result: React.StatelessComponent<any> = (props: any) => (
     <Field {...props}>
       {prs => (
-        <Component {...prs} {...prs.input} />
+        <Component {...prs as any } {...prs.input} />
       )}
     </Field>
   );
