3cf24ef70d094c585948e058beed88e1d0f95b51
Mikhail <dashkevi4mike@gmail.com>
2020-04-17 21:14:06 +0700

    guest dropdown

---
+++ src/shared/helpers/react/getFieldWithComponent.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Field, FieldRenderProps, FieldProps as RFFieldProps } from 'react-final-form';
+// eslint-disable-next-line import/no-unresolved
+import { MergeRight } from '_helpers';
+
+type BaseWrappedFieldProps = FieldRenderProps<any> & {
+  value?: any;
+  onChange?: any;
+};
+
+// keys for pick props from RFFieldProps, Omit dont work because RFFieldProps have index signature
+type RFFieldPropKey =
+  | 'allowNull' | 'format' | 'formatOnBlur' | 'parse' | 'name'
+  | 'isEqual' | 'subscription' | 'validate' | 'value';
+
+function getFieldWithComponent<P extends BaseWrappedFieldProps>(
+  Component: React.ComponentType<P>,
+) {
+  type OwnProps = Omit<P, keyof BaseWrappedFieldProps>;
+  type FieldProps = Pick<RFFieldProps<any>, RFFieldPropKey>;
+  type ResultProps = MergeRight<OwnProps, FieldProps>;
+
+  const result: React.StatelessComponent<ResultProps> = (props: ResultProps) => (
+    <Field {...props}>
+      {prs => (
+        <Component {...prs} {...prs.input} />
+      )}
+    </Field>
+  );
+
+  result.displayName = `FieldWithComponent(${Component.displayName || Component.name || 'Component'})`;
+  return result;
+}
+
+export { getFieldWithComponent };
--- src/shared/view/elements/GuestsDropdown/GuestsDropdown.tsx
+++ src/shared/view/elements/GuestsDropdown/GuestsDropdown.tsx
@@ -3,6 +3,8 @@ import block from 'bem-cn';
 import { autobind } from 'core-decorators';
 import { Form, FormRenderProps } from 'react-final-form';
 
+import { getFieldWithComponent } from 'shared/helpers/react'
+
 import { Dropdown } from '../Dropdown/Dropdown';
 import { NumberInput } from '../NumberInput/NumberInput';
 import { TextButton } from '../TextButton/TextButton';
@@ -17,10 +19,20 @@ type Props = {
 
 type State = {
   isOpen: boolean;
+  adults: number;
+  childs: number;
+  babies: number;
 }
 
+const InputNumberField = getFieldWithComponent(NumberInput);
+
 class GuestsDropdown extends React.Component<Props, State> {
-  public state = { isOpen: false };
+  public state = { 
+    isOpen: false,
+    adults: 0,
+    childs: 0,
+    babies: 0,
+  };
 
   render() {
     const { isOpen } = this.state;
@@ -28,7 +40,7 @@ class GuestsDropdown extends React.Component<Props, State> {
     return (
       <Dropdown
         label="dropdown with form"
-        placeholder="Fill your family"
+        placeholder={this.getDropdownTitle()}
         isOpen={isOpen}
         onChange={this.handleDropdownIsOpenState}
       >
@@ -45,25 +57,25 @@ class GuestsDropdown extends React.Component<Props, State> {
     return (
       <form onSubmit={handleSubmit} className={b()}>
         <div className={b('field')}>
-          <NumberInput
+          <InputNumberField
             name="adults"
             label="Adults"
-            min={1}
+            min={0}
             max={4}
-            initialValue={1}
+            initialValue={0}
           />
         </div>
         <div className={b('field')}>
-          <NumberInput
-            name="childen"
-            label="Children"
+          <InputNumberField
+            name="childs"
+            label="Childen"
             min={0}
             max={5}
             initialValue={0}
           />
         </div>
         <div className={b('field')}>
-          <NumberInput
+          <InputNumberField
             name="babies"
             label="Babies"
             min={0}
@@ -78,11 +90,18 @@ class GuestsDropdown extends React.Component<Props, State> {
     );
   }
 
+  private getDropdownTitle() {
+    const { adults, childs, babies } = this.state;
+    if (!adults && !childs && !babies) { return 'Fill your family'; }
+    return `${adults} adults, ${childs} children, ${babies} babies`;
+  }
+
   @autobind
-  handleFormSubmit(values: { adults: number; children: number; babies: number; }) {
-    const { onChange } = this.props;
-    this.setState({ isOpen: false });
-    if (onChange) {onChange(values); }
+  handleFormSubmit(values: { adults: number; childs: number; babies: number; }) {
+    this.setState({ 
+      isOpen: false, 
+      ...values,
+    });
   }
 
   @autobind
